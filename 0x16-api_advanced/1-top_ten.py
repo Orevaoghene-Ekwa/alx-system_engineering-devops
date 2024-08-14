908814dd88774e5689860c296027c818e52c5f22
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2024-08-14 01:02:33 +0000

    prints the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit."""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
